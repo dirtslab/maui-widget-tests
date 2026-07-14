6adef8a5bdcbcb98ca6c84a72acd9a4eb68a0f75
Alex Andriopoulos <alandriopoulos@entersoftone.gr>
2026-07-14 09:48:07 +0300

    Add project files.

---
+++ widget-tests/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace widget_tests
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ widget-tests/ConfigPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace widget_tests;
+
+public partial class ConfigPage : ContentPage
+{
+	public ConfigPage()
+	{
+		InitializeComponent();
+	}
+}
